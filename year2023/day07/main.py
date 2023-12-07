647aae3309a77d76f6237f4353fa1fbacbdb86e8
Alphee <soulat.nathanael@gmail.com>
2023-12-07 23:43:12 +0100

    Add 2023, day07 part 2

--- year2023/day07/main.py
+++ year2023/day07/main.py
@@ -21,15 +21,25 @@ class HandType(Enum):
 
 
 class Hand:
-    def __init__(self, raw_hand: str, bid: int):
+    def __init__(self, raw_hand: str, bid: int, activate_joker: bool = False):
+        self.joker_activated = activate_joker
         self.cards: tuple[Card, Card, Card, Card, Card] = tuple(
             CARD_BY_LABEL[label] for label in raw_hand
         )
+        if self.joker_activated:
+            self.joker_count = 0
+            for card in self.cards:
+                if card.label == "J":
+                    card.value = 1
+                    self.joker_count += 1
+
         self.bid = bid
         self.handtype = self._get_handtype()
 
     def _get_handtype(self) -> HandType:
         count_per_label = self._count_per_label()
+        if self.joker_activated and self.joker_count:
+            del count_per_label["J"]
         counts = sorted(count_per_label.values())
         if self.has_n_of_a_kind(5, counts):
             return HandType.FIVE_OF_A_KIND
@@ -52,13 +62,21 @@ class Hand:
         return count_per_label
 
     def has_n_of_a_kind(self, n: int, counts: dict[str, int]) -> bool:
-        return n in counts
+        return n in counts or (
+            self.joker_activated
+            and ((max(counts) if counts else 0) + self.joker_count >= n)
+        )
 
     def has_full_house(self, counts: dict[str, int]) -> bool:
-        return counts == [2, 3]
+        return counts == [2, 3] or (
+            self.joker_activated and self.joker_count and len(counts) == 2
+        )
 
     def has_two_pairs(self, counts) -> bool:
-        return counts == [1, 2, 2]
+        return counts == [1, 2, 2] or (
+            self.joker_activated
+            and ((self.joker_count == 1 and len(counts) <= 3) or (self.joker_count > 1))
+        )
 
     def __lt__(self, other: "Hand") -> bool:
         if self.handtype == other.handtype:
@@ -69,6 +87,9 @@ class Hand:
             assert False, "There are two same hands"
         return self.handtype.value < other.handtype.value
 
+    def __str__(self) -> str:
+        return "".join(card.label for card in self.cards)
+
 
 CARD_BY_LABEL = {str(i): Card(str(i), i) for i in range(2, 10)} | {
     label: Card(label, value) for value, label in enumerate("TJQKA", 10)
@@ -77,14 +98,18 @@ CARD_BY_LABEL = {str(i): Card(str(i), i) for i in range(2, 10)} | {
 
 class Day(AbstractDay):
     @classmethod
-    def _resolve_part1(cls, lines: list[str]):
+    def _resolve(cls, lines: list[str], activate_joker: bool):
         hands: list[Hand] = []
         for line in lines:
             raw_hand, bid = line.split(" ")
-            hands.append(Hand(raw_hand, int(bid)))
+            hands.append(Hand(raw_hand, int(bid), activate_joker))
         hands.sort()
         return sum([rank * hand.bid for rank, hand in enumerate(hands, 1)])
 
     @classmethod
+    def _resolve_part1(cls, lines: list[str]):
+        return cls._resolve(lines, activate_joker=False)
+
+    @classmethod
     def _resolve_part2(cls, lines: list[str]):
-        return None
+        return cls._resolve(lines, activate_joker=True)
--- year2023/day07/tests/test_all.py
+++ year2023/day07/tests/test_all.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from ..main import Day
+from ..main import Day, Hand, HandType
 
 tests_dir = Path(__file__).parent
 
@@ -8,4 +8,29 @@ tests_dir = Path(__file__).parent
 def test_day():
     result_part1, result_part2 = Day.resolve(tests_dir / "test01.txt")
     assert result_part1 == 6440, "Part 1 is incorrect"
-    assert result_part2 == 0, "Part 2 is incorrect"
+    assert result_part2 == 5905, "Part 2 is incorrect"
+
+
+raw_hands_for_tests = [
+    # raw hands, expected handtype, expected rank
+    ("26J35", HandType.ONE_PAIR, 1),
+    ("T55J5", HandType.FOUR_OF_A_KIND, 5),
+    ("J888J", HandType.FIVE_OF_A_KIND, 6),
+    ("KAKAK", HandType.FULL_HOUSE, 3),
+    ("2AAAA", HandType.FOUR_OF_A_KIND, 4),
+    ("33333", HandType.FIVE_OF_A_KIND, 7),
+    ("J4452", HandType.THREE_OF_A_KIND, 2),
+]
+
+
+def test_compare_hands():
+    hands: list[Hand] = []
+    for raw_hand, expected_handtype, _ in raw_hands_for_tests:
+        hand = Hand(raw_hand=raw_hand, bid=1, activate_joker=True)
+        assert hand.handtype is expected_handtype, str(hand)
+        hands.append(hand)
+
+    hands.sort()
+    assert all(
+        str(hands[rank - 1]) == raw_hand for raw_hand, _, rank in raw_hands_for_tests
+    )
